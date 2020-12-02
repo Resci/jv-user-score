ae73f79e54c72035f66f9d4f093a5651e4ad0d16
Resci <vIsUAl123>
2020-12-02 14:52:25 +0200

    Add solution

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,16 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.UserNotFoundException;
+
 public class UserService {
 
-    public int getUserScore(String[] records, String email) {
-        return 0;
+    public int getUserScore(String[] records, String email) throws UserNotFoundException {
+        for (String record: records) {
+            String[] recordSplit = record.split(":");
+            if (recordSplit.length == 2 && recordSplit[0].equals(email)) {
+                return Integer.parseInt(recordSplit[1]);
+            }
+        }
+        throw new UserNotFoundException();
     }
 }
